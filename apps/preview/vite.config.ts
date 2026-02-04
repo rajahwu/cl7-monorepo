0cf9a7b5bacece46f4e37dbc4cc5fe744465e496
Vincent Radford <vmradford@gmail.com>
2026-02-04 15:35:22 -0500

    chore: sync apps with new package architecture

--- apps/preview/vite.config.ts
+++ apps/preview/vite.config.ts
@@ -1,6 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      // Direct Vite to the source code for instant updates
+      '@clearline7/set-definitions': path.resolve(__dirname, '../../packages/set-definitions/src'),
+      '@clearline7/theme': path.resolve(__dirname, '../../packages/theme/src'),
+      '@clearline7/components': path.resolve(__dirname, '../../packages/components/src'),
+      '@clearline7/types': path.resolve(__dirname, '../../packages/types/src'),
+    },
+  },
+  server: {
+    fs: {
+      allow: ['../..'], // Allows Vite to climb out of the app folder into the packages folder
+    },
+  },
 })
--- apps/style-guide/vite.config.ts
+++ apps/style-guide/vite.config.ts
@@ -1,15 +1,30 @@
-import mdx from '@mdx-js/rollup'
 import { defineConfig } from 'vite'
+import mdx from '@mdx-js/rollup'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    {enforce: 'pre', ...mdx({/* jsxImportSource: …, otherOptions… */})},
-    react({include: /\.(jsx|js|mdx|md|tsx|ts)$/})
+    {
+      enforce: 'pre',
+      ...mdx({
+        /* jsxImportSource: …, otherOptions… */
+      }),
+    },
+    react({ include: /\.(jsx|js|mdx|md|tsx|ts)$/ }),
   ],
+  resolve: {
+    alias: {
+      // Direct Vite to the source code for instant updates
+      '@clearline7/set-definitions': path.resolve(__dirname, '../../packages/set-definitions/src'),
+      '@clearline7/theme': path.resolve(__dirname, '../../packages/theme/src'),
+      '@clearline7/components': path.resolve(__dirname, '../../packages/components/src'),
+      '@clearline7/types': path.resolve(__dirname, '../../packages/types/src'),
+    },
+  },
+  server: {
+    fs: {
+      allow: ['../..'], // Allows Vite to climb out of the app folder into the packages folder
+    },
+  },
 })
-
-
-
-
